e03e33a5fec6bd73d2e35efc406c7d231deb003a
tluccs <tameezlatib@gmail.com>
2019-03-14 12:33:21 -0700

    Can someone train the autoencoder?

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -5,22 +5,23 @@ import numpy as np
 class Autoencoder:
     def __init__(self, X_train, X_val, X_test):
 
-        self.X_train = X_train.reshape(X_train.shape[0],np.prod(X_train.shape[1:]))
-        self.y_train = X_train.reshape(X_train.shape[0],np.prod(X_train.shape[1:]))
-        self.X_val = X_val.reshape(X_val.shape[0],np.prod(X_val.shape[1:]))
-        self.y_val = X_val.reshape(X_val.shape[0],np.prod(X_val.shape[1:]))
-        self.X_test = X_test.reshape(X_test.shape[0],np.prod(X_test.shape[1:]))
-        self.y_test = X_test.reshape(X_test.shape[0],np.prod(X_test.shape[1:]))
+        self.X_train = X_train
+        self.y_train = X_train
+        self.X_val = X_val
+        self.y_val = X_val
+        self.X_test = X_test
+        self.y_test = X_test
 
         # Initialize Keras model
         self.model = keras.Sequential()
 
-    def train(self, optimizer='adam', epochs=5, encoded_dim=500):
+    def train(self, optimizer='adam', epochs=5, encoded_dim=2200):
         self.model.add(keras.layers.Flatten())
         self.model.add(keras.layers.Dense(encoded_dim, activation='relu'))
-        self.model.add(keras.layers.Dense(22000, activation='sigmoid'))
-
-        self.model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+        self.model.add(keras.layers.Dense(encoded_dim, activation='relu'))
+        self.model.add(keras.layers.Dense(22000))
+        self.model.add(keras.layers.Reshape((22, 1000)))
+        self.model.compile(optimizer=optimizer, loss='mse', metrics=['accuracy'])
         self.model.fit(self.X_train, self.y_train, validation_data=(self.X_val, self.y_val), epochs=epochs)
 
     def evaluate(self):
