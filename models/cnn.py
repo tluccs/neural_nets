376ef035153d2c6eb04c3d87149e0496a98b066e
tluccs <tameezlatib@gmail.com>
2019-03-15 00:51:44 -0700

    45% val acc with RNN using downsample

--- models/cnn.py
+++ models/cnn.py
@@ -2,24 +2,24 @@ import tensorflow as tf
 from tensorflow import keras
 
 class CNN:
-    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test):
+    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test, samples=1000, downsample=1):
 
-        self.X_train = X_train
+        self.X_train = X_train[:,:,:samples:downsample]
         self.y_train = y_train
-        self.X_val = X_val
+        self.X_val = X_val[:,:,:samples:downsample]
         self.y_val = y_val
-        self.X_test = X_test
+        self.X_test = X_test[:,:,:samples:downsample]
         self.y_test = y_test
 
         # Initialize Keras model
         self.model = keras.Sequential()
 
     def train(self, stride=2, optimizer='adam', epochs=5):
-        self.model.add(keras.layers.Conv1D(3, 3, strides=stride, padding='same', data_format='channels_last', use_bias=True, input_shape=(22,1000)))
+        self.model.add(keras.layers.Conv1D(300, 5, strides=stride, padding='same', data_format='channels_last', use_bias=True))
         self.model.add(keras.layers.Activation('relu'))
         self.model.add(keras.layers.MaxPooling1D(pool_size=2, strides=None, padding='same', data_format='channels_last'))
         self.model.add(keras.layers.Activation('relu'))
-        self.model.add(keras.layers.Conv1D(3, 3, strides=stride, padding='same', data_format='channels_last', use_bias=True))
+        self.model.add(keras.layers.Conv1D(50, 3, strides=stride, padding='same', data_format='channels_last', use_bias=True))
         self.model.add(keras.layers.Activation('relu'))
         self.model.add(keras.layers.MaxPooling1D(pool_size=2, strides=None, padding='same', data_format='channels_last'))
         self.model.add(keras.layers.Activation('relu'))
@@ -38,3 +38,6 @@ class CNN:
 
     def evaluate(self):
         return self.model.evaluate(self.X_test, self.y_test, verbose=1)
+
+    def show_model(self):
+        print(self.model.summary())
--- models/rnn.py
+++ models/rnn.py
@@ -8,15 +8,20 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import GRU
 from keras.layers import Dropout
+from keras.layers import Conv1D
+from keras.layers import MaxPooling1D
+from keras.layers import Activation
+from keras.layers import Flatten
+from keras.layers import Permute
+
 
 class RNN:
-    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test):
-
-        self.X_train = X_train
+    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test, samples=1000, downsample=1):
+        self.X_train = X_train[:,:,:samples:downsample]
         self.y_train = y_train
-        self.X_val = X_val
+        self.X_val = X_val[:,:,:samples:downsample]
         self.y_val = y_val
-        self.X_test = X_test
+        self.X_test = X_test[:,:,:samples:downsample]
         self.y_test = y_test
 
         # Initialize Keras model
@@ -26,12 +31,14 @@ class RNN:
         self.history = None
 
     def train(self, RNN_architecture=LSTM, activation="sigmoid", \
-              optimizer='adam', epochs=5, batch_size=64, dropout=None, units=200):
+              optimizer='adam', epochs=5, batch_size=64, dropout=None, units=200, stride=2):
+       
+        self.model.add(Permute((2,1)))
         self.model.add(RNN_architecture(units))
-
+        self.model.add(Activation('relu'))
+        self.model.add(Dense(300, activation='relu'))
         if dropout is not None:
             self.model.add(Dropout(rate=dropout))
-
         self.model.add(Dense(4, activation=activation))
         self.model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
         
@@ -56,4 +63,7 @@ class RNN:
         plt.xlabel('Epoch')
         plt.legend(['Train', 'Val'], loc='upper left')
         plt.show()
+
+    def show_model(self):
+        print(self.model.summary())
         
