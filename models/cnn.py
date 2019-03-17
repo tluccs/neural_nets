f0f61881a8614327de4098298d09917a91397916
HirdayGupta <hirday.gupta@gmail.com>
2019-03-17 04:22:37 -0700

    Add channel pos switching to CNN

--- models/cnn.py
+++ models/cnn.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 
 class CNN:
-    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test, start=0, end=1000, downsample=1):
+    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test, start=0, end=1000, downsample=1, channel_pos='channels_last'):
 
         self.X_train = X_train[:,:,start:end:downsample]
         self.y_train = y_train
@@ -10,6 +10,7 @@ class CNN:
         self.y_val = y_val
         self.X_test = X_test[:,:,start:end:downsample]
         self.y_test = y_test
+        self.channel_pos = channel_pos
         # add back some missing data
         """for i in range(1, int(downsample/4)):
             self.X_train = np.concatenate((self.X_train, X_train[:,:,start+i:end:downsample]), axis=0)
@@ -23,13 +24,13 @@ class CNN:
         self.model = keras.Sequential()
 
     def train(self, stride=2, optimizer='adam', epochs=5):
-        self.model.add(keras.layers.Conv1D(300, 5, strides=stride, padding='same', data_format='channels_last', use_bias=True))
+        self.model.add(keras.layers.Conv1D(300, 5, strides=stride, padding='same', data_format=self.channel_pos, use_bias=True))
         self.model.add(keras.layers.Activation('relu'))
-        self.model.add(keras.layers.MaxPooling1D(pool_size=2, strides=None, padding='same', data_format='channels_last'))
+        self.model.add(keras.layers.MaxPooling1D(pool_size=2, strides=None, padding='same', data_format=self.channel_pos))
         self.model.add(keras.layers.Activation('relu'))
-        self.model.add(keras.layers.Conv1D(50, 3, strides=stride, padding='same', data_format='channels_last', use_bias=True))
+        self.model.add(keras.layers.Conv1D(50, 3, strides=stride, padding='same', data_format=self.channel_pos, use_bias=True))
         self.model.add(keras.layers.Activation('relu'))
-        self.model.add(keras.layers.MaxPooling1D(pool_size=2, strides=None, padding='same', data_format='channels_last'))
+        self.model.add(keras.layers.MaxPooling1D(pool_size=2, strides=None, padding='same', data_format=self.channel_pos))
         self.model.add(keras.layers.Activation('relu'))
         self.model.add(keras.layers.Flatten())
         self.model.add(keras.layers.Dense(500))
