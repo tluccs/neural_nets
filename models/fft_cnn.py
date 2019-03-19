f4f84cea22d475229f65dc58ec75cc5538989666
HirdayGupta <hirday.gupta@gmail.com>
2019-03-18 19:43:17 -0700

    Add FFT & CNN stuffz

---
+++ models/fft_cnn.py
@@ -0,0 +1,209 @@
+import tensorflow as tf
+from tensorflow import keras
+
+class FFT_CNN_1:
+    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test, channel_pos='channels_last', dropout=0.0, use_elu=False, use_batchnorm=False):
+
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_val = X_val
+        self.y_val = y_val
+        self.X_test = X_test
+        self.y_test = y_test
+        self.channel_pos = channel_pos
+        self.dropout=dropout
+        self.use_elu = use_elu
+        self.use_batchnorm = use_batchnorm
+
+        self.history = None
+
+        # Initialize Keras model
+        self.model = keras.Sequential()
+
+    def train(self, stride=1, optimizer='adam', epochs=5):
+        self.model.add(keras.layers.Conv1D(500, 5, strides=stride, padding='same', data_format=self.channel_pos, use_bias=True))
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+        self.model.add(keras.layers.MaxPooling1D(pool_size=2, strides=None, padding='same', data_format=self.channel_pos))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+
+        self.model.add(keras.layers.Conv1D(350, 3, strides=stride, padding='same', data_format=self.channel_pos, use_bias=True))
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+        self.model.add(keras.layers.MaxPooling1D(pool_size=2, strides=None, padding='same', data_format=self.channel_pos))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+
+        self.model.add(keras.layers.Conv1D(200, 2, strides=stride, padding='same', data_format=self.channel_pos, use_bias=True))
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+        self.model.add(keras.layers.MaxPooling1D(pool_size=2, strides=None, padding='same', data_format=self.channel_pos))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+
+        self.model.add(keras.layers.Flatten())
+
+        self.model.add(keras.layers.Dense(500))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+        self.model.add(keras.layers.Dense(150))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+        self.model.add(keras.layers.Dense(100))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dense(4))
+        self.model.add(keras.layers.Activation('softmax'))
+
+        self.model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+        self.history = self.model.fit(self.X_train, self.y_train, validation_data=(self.X_val, self.y_val), epochs=epochs)
+
+    def evaluate(self):
+        return self.model.evaluate(self.X_test, self.y_test, verbose=1)
+
+    def show_model(self):
+        print(self.model.summary())
+
+    def get_loss_history(self):
+        if self.history is None:
+            return
+        history = self.history
+        return history.history['loss'], history.history['val_loss']
+
+class FFT_FCNET:
+    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test, dropout=0.0, use_elu=False, use_batchnorm=False):
+
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_val = X_val
+        self.y_val = y_val
+        self.X_test = X_test
+        self.y_test = y_test
+        self.dropout=dropout
+        self.use_elu = use_elu
+        self.use_batchnorm = use_batchnorm
+
+        # Initialize Keras model
+        self.model = keras.Sequential()
+
+    def train(self, optimizer='adam', epochs=5):
+        self.model.add(keras.layers.Dense(500))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+
+        self.model.add(keras.layers.Dense(500))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+
+        self.model.add(keras.layers.Dense(350))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+
+        self.model.add(keras.layers.Dense(200))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+
+        self.model.add(keras.layers.Dense(4))
+        self.model.add(keras.layers.Activation('softmax'))
+
+        self.model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+        self.history = self.model.fit(self.X_train, self.y_train, validation_data=(self.X_val, self.y_val), epochs=epochs)
+
+    def evaluate(self):
+        return self.model.evaluate(self.X_test, self.y_test, verbose=1)
+
+    def show_model(self):
+        print(self.model.summary())
+
+    def get_loss_history(self):
+        if self.history is None:
+            return
+        history = self.history
+        return history.history['loss'], history.history['val_loss']
+
+
+class FFT_CNN_2:
+    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test, channel_pos='channels_last',dropout=0.0,use_elu=False,use_batchnorm=False):
+
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_val = X_val
+        self.y_val = y_val
+        self.X_test = X_test
+        self.y_test = y_test
+        self.channel_pos = channel_pos
+        self.dropout=dropout
+        self.use_elu = use_elu
+        self.use_batchnorm = use_batchnorm
+
+        # Initialize Keras model
+        self.model = keras.Sequential()
+
+    def train(self, stride=1, optimizer='adam', epochs=5):
+        # self.model.add(keras.layers.Conv2DTranspose(128, 25, padding='valid', input_shape=(5, 5, 64)))
+        # if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        # self.model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+        # self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        # self.model.add(keras.layers.Dropout(self.dropout))
+
+        self.model.add(keras.layers.Conv2D(512, 2, strides=stride))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        # self.model.add(keras.layers.MaxPooling2D(pool_size=(1, 1)))
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+
+        self.model.add(keras.layers.Conv2D(256, 2, strides=stride))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        # self.model.add(keras.layers.MaxPooling2D(pool_size=(1, 1)))
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+
+        self.model.add(keras.layers.Conv2D(128, 1, strides=stride))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        # self.model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+
+        self.model.add(keras.layers.Flatten())
+
+        self.model.add(keras.layers.Dense(1024))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+
+        self.model.add(keras.layers.Dense(512))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+
+        self.model.add(keras.layers.Dense(256))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+
+        self.model.add(keras.layers.Dense(100))
+        if self.use_batchnorm: self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.Activation('elu' if self.use_elu else 'relu'))
+        self.model.add(keras.layers.Dropout(self.dropout))
+
+        self.model.add(keras.layers.Dense(4))
+        self.model.add(keras.layers.Activation('softmax'))
+
+        self.model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+        self.history = self.model.fit(self.X_train, self.y_train, validation_data=(self.X_val, self.y_val), epochs=epochs)
+
+    def evaluate(self):
+        return self.model.evaluate(self.X_test, self.y_test, verbose=1)
+
+    def show_model(self):
+        print(self.model.summary())
+
+    def get_loss_history(self):
+        if self.history is None:
+            return
+        history = self.history
+        return history.history['loss'], history.history['val_loss']
--- utils/utilities.py
+++ utils/utilities.py
@@ -38,9 +38,21 @@ def get_more_trials(X_data, y_data, Ns=[300,350,400]):
 		y_train_total = np.concatenate((y_train_total, y_data), axis=0)
 	return X_train_total, y_train_total
 
-def fft_electrode_data(X):
-    L = 4000 # Signal length
-    Fs = 250 # sampling freq
+def fft_electrode_data(X, Fs=250, L=4000):
+    """
+    Runs an FFT on input data
+
+    Parameters
+    --------------------
+        X     -- numpy array of shape (t,) - data with t timesteps
+        Fs    -- the sampling frequency
+        L     -- the length of the sample in milliseconds
+
+    Returns
+    --------------------
+        Px -- the PSD of X - obtained by running an FFT and running some additional processing steps
+        f  -- the frequencies that each of the Px corresponds to
+    """
     NFFT = Fs//2
 
     X = np.fft.fft(X, NFFT)
@@ -52,3 +64,109 @@ def fft_electrode_data(X):
     Px = Px[0:f.shape[0]]
 
     return Px, f
+
+def fft_and_reshape(X, Fs=250, L=4000):
+    """
+    Runs fft on the last axis (assuming that is the timestep axis)
+
+    Parameters
+    --------------------
+        X     -- numpy array of shape (N, E, T) - data N examples, E electrodes and T timesteps
+        Fs    -- the sampling frequency
+        L     -- the length of the sample in milliseconds
+
+    Returns
+    --------------------
+        X_fft -- numpy array of shape (N,C,E) - changing the last axis to C features (C = num_freqs = Fs//4 + 2) and then swapping C & E.
+    """
+    num_freqs = Fs//4 + 2
+    X_fft = np.zeros((X.shape[0], X.shape[1], num_freqs))
+
+    N,E,_ = X.shape
+
+    for i in np.arange(N):
+        for j in np.arange(E):
+            EEG_trial = X[i][j]
+            EEG_trial_FFT,_ = fft_electrode_data(EEG_trial)
+            X_fft[i][j] = EEG_trial_FFT
+
+    return X_fft.swapaxes(1,2)
+
+def reshape_fft_1D(X_fft):
+    """
+    Runs fft on the last axis (assuming that is the timestep axis)
+
+    Parameters
+    --------------------
+        X_fft -- numpy array of shape (N, C, E) - data N examples, C channels & E electrodes.
+
+    Returns
+    --------------------
+        X_fft_sp -- numpy array of shape (N, C, H, W) - changing the last axis to P power densities (P = num_freqs = Fs//4 + 2)
+    """
+    N,C,E= X_fft.shape
+    return X_fft.reshape(N,C*E)
+
+def reshape_fft_spatial(X_fft):
+    """
+    Runs fft on the last axis (assuming that is the timestep axis)
+
+    Parameters
+    --------------------
+        X_fft -- numpy array of shape (N, C, E) - data N examples, C channels & E electrodes.
+
+    Returns
+    --------------------
+        X_fft_sp -- numpy array of shape (N, C, H, W) - changing the last axis to P power densities (P = num_freqs = Fs//4 + 2)
+    """
+    # num_freqs = Fs//4 + 2
+    # X_fft = np.zeros((X.shape[0], X.shape[1], num_freqs))
+
+    X_fft = X_fft.swapaxes(2,1) # X_fft is now (N, E, C) again
+    N,E,C = X_fft.shape
+
+    H,W = 5,5
+
+    X_fft_sp = np.zeros((N, H, W, C))
+
+    feature_map = {
+        '0,2': 0,
+
+        '1,0': 1,
+        '1,1': 2,
+        '1,2': 3,
+        '1,3': 4,
+        '1,4': 5,
+
+        '2,0': 7,
+        '2,1': 8,
+        '2,2': 9,
+        '2,3': 10,
+        '2,4': 11,
+
+        '3,0': 13,
+        '3,1': 14,
+        '3,2': 15,
+        '3,3': 16,
+        '3,4': 17,
+
+        '4,1': 18,
+        '4,2': 19,
+        '4,3': 20,
+
+        '5,2': 21
+    }
+
+    for i in np.arange(N):
+        curr_ind  = X_fft[i]
+        X_fft_sp_i = np.zeros((H, W, C))
+
+        for h in np.arange(H):
+            for w in np.arange(W):
+                fmap_idx = "{},{}".format(h,w)
+                if fmap_idx in feature_map:
+                    X_fft_sp_i[h][w] = curr_ind[feature_map[fmap_idx]]
+
+        X_fft_sp[i] = X_fft_sp_i
+
+    return X_fft_sp
